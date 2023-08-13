8ab0b0de12455e587cafb2278b73b0677b865005
Gary Woodfine <gary.woodfine@threenine.co.uk>
2023-08-13 23:28:35 +0100

    Theme switcher initial

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 export default defineNuxtConfig({
-  devtools: {enabled: true},
-  modules: [ '@nuxtjs/tailwindcss'],
+  devtools: {
+    enabled: true,
 
+    timeline: {
+      enabled: true,
+    },
+  },
+  modules: [ '@nuxtjs/tailwindcss', 'nuxt-icon', '@nuxtjs/color-mode',  'nuxt-headlessui'],
+  // module::color-mode
+  colorMode: {
+    classSuffix: '',
+  },
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,10 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import ThemeSwitcher from "~/layouts/theme-switcher.vue";
+</script>
 
 <template>
   <div>
-
+   <theme-switcher type="select-box" />
     <span class="text-orange text-5xl">PayBolt</span>
 
   </div>
