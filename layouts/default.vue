1269b3342799fff617833b18b80549a0dcddbb40
Gary Woodfine <gary.woodfine@threenine.co.uk>
2023-08-16 16:29:36 +0100

    basic theme-switch implemented

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import Navbar from "~/components/layout/navbar.vue";
+</script>
+
+<template>
+
+  <div>
+    <navbar/>
+
+    <slot />
+
+
+
+  </div>
+
+</template>
+
+<style scoped></style>
